07729e986595235ea03e367c8ea61dea69871cc8
Specvey <you@example.com>
2016-11-02 11:33:46 +0100

    toString() OK

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,198 @@
+// ObjectModelLab\src\model\index.js
 
-/* TODO : Créer le modèle objet ici */ 
+// Soit faire un export const des class
+
+// Soit créer des fonctions 
+
+class Data
+{
+	constructor(donnees)
+	{
+		this._values = donnees['values'] || donnees['value'];
+		this._labels = donnees['labels'] || null;
+	}
+
+	get values()
+	{
+    	return this._values;
+  	}
+  	
+  	set values(val)
+  	{
+    	this._values = val;
+  	}
+  	
+  	get labels()
+  	{
+    	return this._labels;
+  	}
+
+  	set labels(lab)
+  	{
+    	this._labels = lab;
+  	}
+
+  	toString()
+  	{
+  		if(this._labels === null) return ''+this._values+'\n';
+  		let s = '';
+  		for(let i = 0; i < this._values.length; i++)
+  		{
+  			s+=''+this._labels[i]+' '+this._values[i]+'\n';
+  		}
+  		return s;
+  	}
+}
+
+
+class Sensor
+{
+	constructor(id, name, data)
+	{
+		this._id = id || '';
+		this._name = name || '';
+		this._data = new Data(data);
+	}
+
+	get id()
+	{
+    	return this._id;
+  	}
+  	
+  	set id(identifiant)
+  	{
+    	this._id = identifiant;
+  	}
+  	
+  	get name()
+  	{
+    	return this._name;
+  	}
+
+  	set name(nom)
+  	{
+    	this._name = nom;
+  	}
+
+  	get data()
+  	{
+    	return this._data;
+  	}
+
+  	set data(donnees)
+  	{
+    	this._data = donnees;
+  	}
+
+  	toString()
+  	{
+  		return ('id : '+this._id+'\nname : '+this._name+'\n'+this._data.toString());
+  	}
+}
+
+class Temperature extends Sensor
+{
+	constructor(id, name, data)
+	{
+		super(id, name, data);
+	}
+
+	toString()
+  	{
+  		let s='TEMPERATURE\n'+super.toString();
+  		return s;
+  	}
+}
+
+class Humidity extends Sensor
+{
+	constructor(id, name, data)
+	{
+		super(id, name, data);
+	}
+
+	toString()
+  	{
+  		let s='HUMIDITY\n'+super.toString();
+  		return s;
+  	}
+}
+
+class Light extends Sensor
+{
+	constructor(id, name, data)
+	{
+		super(id, name, data);
+	}
+
+	toString()
+  	{
+  		let s='LIGHT\n'+super.toString();
+  		return s;
+  	}
+}
+
+class Switch extends Sensor
+{
+	constructor(id, name, data)
+	{
+		super(id, name, data);
+	}
+
+	toString()
+  	{
+  		let s='SWITCH\n'+super.toString();
+  		return s;
+  	}
+}
+
+class Door extends Sensor
+{
+	constructor(id, name, data)
+	{
+		super(id, name, data);
+	}
+
+	toString()
+  	{
+  		let s='DOOR\n'+super.toString();
+  		return s;
+  	}
+}
+
+
+class Fan_Speed extends Sensor
+{
+  constructor(id, name, data)
+  {
+    super(id, name, data);
+  }
+
+  toString()
+    {
+      let s='FAN_SPEED\n'+super.toString();
+      return s;
+    }
+}
+/*
+let data = new Data(0);
+
+let temp = new Temperature('#0123', 'JeSuisUneT',data);
+
+console.log(temp.toString());
+*/
+
+export const temperatureTest = (donnees) => {
+  let temperature = new Temperature(donnees['id'],donnees['name'],donnees['data']);
+  return temperature.toString();
+};
+
+export const porteTest = (donnees) => {
+  let porte = new Door(donnees['id'],donnees['name'],donnees['data']);
+  return porte.toString();
+};
+
+export const ventilateurTest = (donnees) => {
+  let ventilateur = new Fan_Speed(donnees['id'],donnees['name'],donnees['data']);
+  return ventilateur.toString();
+};
--- test/model/index.js
+++ test/model/index.js
@@ -1,10 +1,45 @@
+// ObjectModelLab\test\model\index.js
+
 import expect from 'expect';
 
-// import { ??? } from '../../src/model';
+import
+{
+	temperatureTest,
+	porteTest,
+	ventilateurTest,
+} from '../../src/model';
 
-import  { data }  from './sensors_data'
+import
+{
+	data
+} from './sensors_data';
+
 console.log(data.length);
 
-describe('Sensor model tests', () => {
-  /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
+describe('Object Model Lab Tests', () => {
+
+	describe('Température tests', () => {
+
+		it('Object température bien créé', () => {
+			expect(temperatureTest(data[0])).toEqual('TEMPERATURE\n'+'id : '+'1234'+'\nname : '+'Température Bureau'+'\n'+'2016-10-19T08:00:00.000Z 23\n'+'2016-10-19T09:00:00.000Z 23\n'+'2016-10-19T10:00:00.000Z 22\n'+'2016-10-19T11:00:00.000Z 21\n'+'2016-10-19T12:00:00.000Z 23\n'+'2016-10-19T13:00:00.000Z 23\n'+'2016-10-19T14:00:00.000Z 23\n'+'2016-10-19T15:00:00.000Z 25\n'+'2016-10-19T16:00:00.000Z 25\n');
+    	});
+
+	});
+
+	describe('Porte tests', () => {
+  	
+  		it('Object porte bien créé', () => {
+			expect(porteTest(data[1])).toEqual('DOOR\n'+'id : '+'10245'+'\nname : '+'Porte du Garage'+'\n'+'0\n');
+    	});
+
+	});
+
+	describe('Ventilateur tests', () => {
+  	
+  		it('Object ventilateur bien créé', () => {
+			expect(ventilateurTest(data[2])).toEqual('FAN_SPEED\n'+'id : '+'2222'+'\nname : '+'Ventilateur Ordinateur Bureau'+'\n'+'2016-10-19T10:00:00.000Z 1073\n'+'2016-10-19T10:05:00.000Z 1800\n'+'2016-10-19T10:10:00.000Z 2299\n'+'2016-10-19T10:15:00.000Z 2176\n'+'2016-10-19T10:20:00.000Z 1899\n'+'2016-10-19T10:25:00.000Z 1400\n');
+    	});
+
+	});
+
 });
